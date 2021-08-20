5ca07c70ddaee26c42cbf190f15f8e35bea20ad2
the-macson <ak9991927952@gmail.com>
2021-08-20 16:35:23 +0530

    day one of python class

--- Semester 3/INT 213/Code/Lecture 0/index.py
+++ Semester 3/INT 213/Code/Lecture 0/index.py
@@ -1,1 +1,4 @@
-print("hello world")
+print("hello world") #print is used for printing 
+x = 10; #declaring x varible and also initialising x = 10 
+type(x)  # using type we can find data type of x varible
+print(type(x)) # printing class type of x
